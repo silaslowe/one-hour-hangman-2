c457ba0492b71bfcf1e91ba8ee3ce6c0340cc963
negative1skunk@gmail.com <silaslowe@gmail.com>
2021-05-22 14:25:49 -0500

    wrongArray updates correctly

--- src/game.js
+++ src/game.js
@@ -4,7 +4,7 @@ import {Letter} from './letter'
 import {WordContext} from './wordProvider'
 
 export const Game = ({word}) => {
-    const {guessArray, addGuess} = useContext(WordContext)
+    const {guessArray, addGuess, wrongArray} = useContext(WordContext)
     const guess = useRef(null)
 
     
@@ -24,5 +24,6 @@ export const Game = ({word}) => {
             word.split("").map((l, i) => <Letter key={i} letter={l} guessArray={guessArray}/>)
             }
             </div>
+            <Guesses wrongArray={wrongArray} />
     </>
 }
