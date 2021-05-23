033fcbfc8379634ccd9161dd505758b48c89d912
negative1skunk@gmail.com <silaslowe@gmail.com>
2021-05-22 20:12:37 -0500

    win and lose conditons work now

--- src/game.js
+++ src/game.js
@@ -4,18 +4,20 @@ import {Letter} from './letter'
 import {WordContext} from './wordProvider'
 
 export const Game = ({word}) => {
-    const {guessArray, addGuess, wrongArray, guessCount} = useContext(WordContext)
+    const {guessArray, addGuess, wrongArray, guessCount, wordSet} = useContext(WordContext)
     const guess = useRef(null)
 
     return <>
-        <form onSubmit={(e) => addGuess(e, guess)} style={{width: "10%", margin: "2rem"}}>
+        <form onSubmit={(e) => addGuess(e, guess)} style={{width: "10%", margin: "0  0 1rem 0"}}>
+            {wrongArray.length > 5 ? <h2>You Lose Dummy!</h2> : null}
+            {guessArray.sort((a,b) => a+b).join() === wordSet.sort((a,b) => a+b).join() ? <h2>You Win Dummy!</h2> : null}
             <label>Guess</label>
             <input 
             type="text"
             ref={guess}
             ></input>
-            <button
-            >Make a guess</button>
+
+            <button disabled={wrongArray.length > 5 || guessArray.sort((a,b) => a+b).join() === wordSet.sort((a,b) => a+b).join()}>Make a guess</button>      
         </form>
 
         <div style={{display: "flex", flexDirection: "row", justifyContent: "center", border: "3px solid black", width: "65%"}}>
